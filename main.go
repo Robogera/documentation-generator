34134cbad6ed3d185cea475d2d784836612ff25e
g.cherdakly <g.cherdakly@int-sys.ru>
2023-02-01 01:17:54 +0300

    minimal working example

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import (
     "log"
     "os"
-    "html/template"
+    "text/template"
+    "bytes"
+    "path/filepath"
 )
+
+type TemplateContents struct {
+    Stylesheet []byte
+    Title string
+    Content string
+}
 
 func main() {
     var tmpl *template.Template
@@ -16,13 +24,33 @@ func main() {
         log.Panicf("Can't read main template: %s", err)
     }
 
+    var stylesheets []os.DirEntry
+    var stylesheet_combined [][]byte = [][]byte{}
+    stylesheets, err = os.ReadDir("./css")
+    for _, stylesheet := range stylesheets {
+        var stylesheet_contents []byte
+        stylesheet_contents, err = os.ReadFile(filepath.Join("./css",stylesheet.Name()))
+        if err != nil {
+            log.Printf("Error with %s: %s", stylesheet.Name(), err)
+            continue
+        }
+        stylesheet_combined = append(stylesheet_combined, stylesheet_contents)
+    }
+
+    var contents *TemplateContents = &TemplateContents{
+        Stylesheet: bytes.Join(stylesheet_combined, []byte{byte('\n')}),
+        Title: "Penis",
+        Content: "Quality content",
+    }
+    log.Printf("%s", bytes.Join(stylesheet_combined, []byte{byte('\n')}))
+
     dest, err = os.Create("./output/index.html")
     if err != nil {
         log.Panicf("Can't create output file: %s", err)
     }
     defer dest.Close()
 
-    err = tmpl.Execute(dest, nil)
+    err = tmpl.Execute(dest, contents)
     if err != nil {
         log.Panicf("Can't write output file: %s", err)
     }
