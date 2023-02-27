ddf9760ffc728f5c036664bf22c096faac4d3096
g.cherdakly <robogera@gmail.com>
2023-02-27 20:23:34 +0300

    Dev: now works with lists

--- src/list.go
+++ src/list.go
@@ -8,6 +8,14 @@ type List struct {
 }
 
 func (list List) Serve() ([]byte, error) {
-    return nil, nil
+    paragraphs := make([][]byte, len(list.Paragraphs))
+    for i, paragraph := range list.Paragraphs {
+        contents, err := paragraph.Serve()
+        if err != nil {
+            continue
+        }
+        paragraphs[i] = contents
+    }
+    return serve(paragraphs, `<ol>{{ range . }}<li>{{ printf "%s" . }}</li>{{ end }}</ol>`)
 }
 
